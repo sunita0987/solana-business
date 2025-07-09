7f76be3f8b411a4005f24d47f980fbb800ed85fc
sunita0987 <sunita0987546@gmail.com>
2025-07-09 09:45:35 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import React from "react";
+import "./App.css";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+        <h1 className='text-5xl font-bold'> hello world</h1>
+      
+    </div>
+  )
+}
+
+export default NavBar
